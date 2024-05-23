4e072de86d38ae8e42aabfa5aab85bc3783eb0e7
sahar <sahar.samadi1376@gmail.com>
2024-05-23 08:10:04 +0330

    updated app.css and app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './pages/Home'
+import Header from './components/Header'
+import BackToTop from './components/BackToTop'
+import { Route, Routes } from 'react-router-dom'
+import RulesDetailPage from './pages/RulesDetailPage'
+import AboutUs from './pages/AboutUs'
+import TenseVerbs from './pages/TenseVerbs'
+import FunSentencesPage from './pages/FunSentencesPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Routes>
+       <Route path='/' element={<Home/>} />
+       <Route path='/about-us' element={<AboutUs/>} />
+       <Route path='/rules-details/:category' element={<RulesDetailPage/>} />
+       <Route path='/tense-Verbs' element={<TenseVerbs/>} />
+       <Route path='/functional-sentences/:category' element={<FunSentencesPage/> } />
+      </Routes>
+      <BackToTop/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import RulesProvider from "./context/RulesContext";
+import IrregularVProvider from "./context/IrregularVContext";
+import TenseProvider from "./context/TenseContext";
+import FunSentencesProvider from "./context/FunSentencesContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <FunSentencesProvider>
+    <TenseProvider>
+      <IrregularVProvider>
+        <RulesProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </RulesProvider>
+      </IrregularVProvider>
+    </TenseProvider>
+  </FunSentencesProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
