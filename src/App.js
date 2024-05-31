efc09b15923cc22af5fc503c31c697d135ddf7fb
sahar9776 <sahar.samadi1376@gmail.com>
2024-05-31 20:56:46 +0430

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import Home from './pages/Home'
-import Header from './components/Header'
-import BackToTop from './components/BackToTop'
-import { Route, Routes } from 'react-router-dom'
-import RulesDetailPage from './pages/RulesDetailPage'
-import AboutUs from './pages/AboutUs'
-import TenseVerbs from './pages/TenseVerbs'
-import FunSentencesPage from './pages/FunSentencesPage'
+import React from "react";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import BackToTop from "./components/BackToTop";
+import { Route, Routes } from "react-router-dom";
+import RulesDetailPage from "./pages/RulesDetailPage";
+import AboutUs from "./pages/AboutUs";
+import TenseVerbs from "./pages/TenseVerbs";
+import FunSentencesPage from "./pages/FunSentencesPage";
+import Login from "./pages/Login";
+import LoginBtn from "./components/LoginBtn";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <LoginBtn />
       <Routes>
-       <Route path='/' element={<Home/>} />
-       <Route path='/about-us' element={<AboutUs/>} />
-       <Route path='/rules-details/:category' element={<RulesDetailPage/>} />
-       <Route path='/tense-Verbs' element={<TenseVerbs/>} />
-       <Route path='/functional-sentences/:category' element={<FunSentencesPage/> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/rules-details/:category" element={<RulesDetailPage />} />
+        <Route path="/tense-Verbs" element={<TenseVerbs />} />
+        <Route path="/functional-sentences/:category" element={<FunSentencesPage />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
-      <BackToTop/>
+      <BackToTop />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
